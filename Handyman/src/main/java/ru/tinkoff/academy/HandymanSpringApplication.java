2b04948e8a080c39b0c7e00bb27458036c7869bb
ut1Puti <sololasuklina@gmail.com>
2023-02-14 00:34:36 +0500

    add actuator and prometeus to LandscapeService

--- Handyman/src/main/java/ru/tinkoff/academy/HandymanSpringApplication.java
+++ Handyman/src/main/java/ru/tinkoff/academy/HandymanSpringApplication.java
@@ -2,8 +2,10 @@ package ru.tinkoff.academy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.metrics.export.prometheus.EnablePrometheusMetrics;
 
 @SpringBootApplication
+@EnablePrometheusMetrics
 public class HandymanSpringApplication {
     public static void main(String[] args) {
         SpringApplication.run(HandymanSpringApplication.class, args);
--- Rancher/src/main/java/ru/tinkoff/academy/actuator/ActuatorController.java
+++ Rancher/src/main/java/ru/tinkoff/academy/actuator/ActuatorController.java
@@ -1,6 +1,7 @@
 package ru.tinkoff.academy.actuator;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/actuator")
 @RequiredArgsConstructor
 public class ActuatorController {
+    private final BuildProperties buildProperties;
+
     @GetMapping("/info")
-    public String info() {
-        return null;
+    public BuildProperties info() {
+        return buildProperties;
     }
 }
