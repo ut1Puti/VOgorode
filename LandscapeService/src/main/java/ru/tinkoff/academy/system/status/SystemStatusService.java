6967f3d185dc07bddbe156d22382a19e065adf51
ut1puti <sololasuklina@gmail.com>
2023-03-01 23:24:43 +0500

    add logging to forceMalfunction

--- LandscapeService/src/main/java/ru/tinkoff/academy/system/status/SystemStatusService.java
+++ LandscapeService/src/main/java/ru/tinkoff/academy/system/status/SystemStatusService.java
@@ -1,12 +1,17 @@
 package ru.tinkoff.academy.system.status;
 
 import lombok.Getter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SystemStatusService {
     @Getter
     private SystemStatus systemStatus = SystemStatus.OK;
+
+
+    private final Logger logger = LoggerFactory.getLogger(SystemStatusService.class);
 
     /**
      * Change system status to Malfunction defined on value of {@code isChangeTo}
@@ -15,6 +20,12 @@ public class SystemStatusService {
      *                   if {@code false} change status to {@link SystemStatus#OK}
      */
     public synchronized void changeToMalfunction(boolean isChangeTo) {
+        SystemStatus oldSystemStatus = systemStatus;
         systemStatus = isChangeTo ? SystemStatus.MALFUNCTION : SystemStatus.OK;
+        logger.info(createChangeSystemStatusLogString(oldSystemStatus, systemStatus));
+    }
+
+    private String createChangeSystemStatusLogString(SystemStatus oldStatus, SystemStatus newStatus) {
+        return String.format("Change from %s to %s", oldStatus, newStatus);
     }
 }
--- RancherService/src/main/java/ru/tinkoff/academy/system/status/SystemStatusService.java
+++ RancherService/src/main/java/ru/tinkoff/academy/system/status/SystemStatusService.java
@@ -1,6 +1,8 @@
 package ru.tinkoff.academy.system.status;
 
 import lombok.Getter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -8,13 +10,22 @@ public class SystemStatusService {
     @Getter
     private SystemStatus systemStatus = SystemStatus.OK;
 
+
+    private final Logger logger = LoggerFactory.getLogger(SystemStatusService.class);
+
     /**
      * Change system status to Malfunction defined on value of {@code isChangeTo}
      *
      * @param isChangeTo if {@code true} change status to {@link SystemStatus#MALFUNCTION},
      *                   if {@code false} change status to {@link SystemStatus#OK}
      */
-    public void changeToMalfunction(boolean isChangeTo) {
+    public synchronized void changeToMalfunction(boolean isChangeTo) {
+        SystemStatus oldSystemStatus = systemStatus;
         systemStatus = isChangeTo ? SystemStatus.MALFUNCTION : SystemStatus.OK;
+        logger.info(createChangeSystemStatusLogString(oldSystemStatus, systemStatus));
+    }
+
+    private String createChangeSystemStatusLogString(SystemStatus oldStatus, SystemStatus newStatus) {
+        return String.format("Change from %s to %s", oldStatus, newStatus);
     }
 }
